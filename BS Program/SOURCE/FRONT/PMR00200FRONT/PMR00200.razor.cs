df254231fdafa9d57153a4f4c71787f573eeb115
gabrielhalsya <gabrielm1603@gmail.com>
2024-06-05 11:00:12 +0700

    new SPR PM sales

--- BS Program/SOURCE/FRONT/PMR00200FRONT/PMR00200.razor.cs
+++ BS Program/SOURCE/FRONT/PMR00200FRONT/PMR00200.razor.cs
@@ -27,6 +27,7 @@ namespace PMR00200FRONT
         [Inject] IClientHelper _clientHelper { get; set; }
         [Inject] private R_ILocalizer<Resources_Dummy_Class> _localizer { get; set; }
         [Inject] private R_IReport _reportService { get; set; }
+
         private R_RadioGroup<ReportTypeDTO, string> _radioReportType;
 
         protected override async Task R_Init_From_Master(object poParameter)
