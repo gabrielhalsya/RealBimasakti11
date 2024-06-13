e65c55eb9a0f29b5758eea939f46ae61c65b4a52
gabrielhalsya <gabrielm1603@gmail.com>
2024-06-13 12:15:51 +0700

    PMR00600 Print summary by tenant

--- BS Program/SOURCE/COMMON/LM/PMR00600COMMON/DTO's/Print/PMR00600DataDTO.cs
+++ BS Program/SOURCE/COMMON/LM/PMR00600COMMON/DTO's/Print/PMR00600DataDTO.cs
@@ -5,5 +5,13 @@ namespace PMR00600COMMON
 {
     public class PMR00600DataDTO : PMR00600SPResultDTO
     {
+        public decimal NSUM_AMOUNT
+        {
+            get
+            {
+                return NJAN_AMOUNT + NFEB_AMOUNT + NMAR_AMOUNT + NAPR_AMOUNT + NMAY_AMOUNT + NJUN_AMOUNT +
+                       NJUL_AMOUNT + NAUG_AMOUNT + NSEP_AMOUNT + NOCT_AMOUNT + NNOV_AMOUNT + NDEC_AMOUNT;
+            }
+        }
     }
 }
