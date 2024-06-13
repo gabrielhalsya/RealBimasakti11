fa5c0c2a42fd49e1ecac0546be62219f0c91d1fb
gabrielhalsya <gabrielm1603@gmail.com>
2024-06-13 20:45:59 +0700

    fix sum PMR00600

--- BS Program/SOURCE/COMMON/LM/PMR00600COMMON/Model/PMR00601DummyData.cs
+++ BS Program/SOURCE/COMMON/LM/PMR00600COMMON/Model/PMR00601DummyData.cs
@@ -58,196 +58,66 @@ namespace PMR00600COMMON.Model
                 LTENANT = true
 
             };
-            loRtn.ReportDataDTO.Data = new List<PMR00601DataDTO>()
+            loRtn.ReportDataDTO.Data = new List<PMR00601DataDTO>();
+
+            var tenants = new List<string> { "Muhammad Andika Putra", "Aurelius Irvin" };
+            var invoices = new List<string> { "INV-001", "INV-002", "INV-003", "INV-004" };
+            var agreements = new List<string> { "AGR-001", "AGR-002", "AGR-003", "AGR-004" };
+            var charges = new List<string> { "CHG01", "CHG02" };
+
+            int amount = 100000000;
+
+            for (int b = 1; b <= 2; b++) // Buildings
             {
-                new PMR00601DataDTO
+                for (int d = 1; d <= 2; d++) // Departments
                 {
-                    CCOMPANY_ID = "COMP01",
-                    CPROPERTY_ID = "PROP01",
-                    CBUILDING_ID = "BLD01",
-                    CBUILDING_NAME = "Building Jakarta",
-                    CDEPT_CODE = "DPT_A",
-                    CDEPT_NAME = "Department Jakarta Pusat",
-                    CTENANT_ID = "MU01",
-                    CTENANT_NAME = "Muhammad Andika Putra",
-                    CINVOICE_NO = "INV-REF-AGRE-1",
-                    CINVOICE_DATE = "1 Jan 2024",
-                    NINVOICE_AMOUNT = 100000000,
-                    NINVOICE_TAX_AMOUNT = 100000000,
-                    NINVOICE_TOTAL_AMOUNT = 200000000,
-                    CAGREEMENT_NO = "REF-AGRE-1",
-                    CAGREEMENT_DATE = "1 Jan 2024",
-                    CCHARGE_ID = "CHG01",
-                    CCHARGE_NAME = "Overtime Weekday(s)",
-                    CUNIT_ID = "Unit_A",
-                    CUNIT_NAME = "Unit Sederhana",
-                    COVERTIME_DATE = "1 Jan 2024",
-                    CTIME_IN = "18:00",
-                    CTIME_OUT = "21:00",
-                    CTENURE = "3H 0M",
-                    NUNIT_PRICE = 100000000,
-                    NUNIT_ACTUAL_AMOUNT = 100000000,
-                    CNOTE = "Every Hour",
-                    CTRANS_STATUS_CODE = "01",
-                    CTRANS_STATUS_NAME = "Closed"
-                },
-                new PMR00601DataDTO
-                {
-                    CCOMPANY_ID = "COMP01",
-                    CPROPERTY_ID = "PROP01",
-                    CBUILDING_ID = "BLD01",
-                    CBUILDING_NAME = "Building Jakarta",
-                    CDEPT_CODE = "DPT_A",
-                    CDEPT_NAME = "Department Jakarta Pusat",
-                    CTENANT_ID = "MU01",
-                    CTENANT_NAME = "Muhammad Andika Putra",
-                    CINVOICE_NO = "INV-REF-AGRE-1",
-                    CINVOICE_DATE = "1 Jan 2024",
-                    NINVOICE_AMOUNT = 100000000,
-                    NINVOICE_TAX_AMOUNT = 100000000,
-                    NINVOICE_TOTAL_AMOUNT = 200000000,
-                    CAGREEMENT_NO = "REF-AGRE-1",
-                    CAGREEMENT_DATE = "1 Jan 2024",
-                    CCHARGE_ID = "CHG01",
-                    CCHARGE_NAME = "Overtime Weekday(s)",
-                    CUNIT_ID = "Unit_A",
-                    CUNIT_NAME = "Unit Sederhana",
-                    COVERTIME_DATE = "1 Jan 2024",
-                    CTIME_IN = "18:00",
-                    CTIME_OUT = "21:00",
-                    CTENURE = "3H 0M",
-                    NUNIT_PRICE = 100000000,
-                    NUNIT_ACTUAL_AMOUNT = 100000000,
-                    CNOTE = "Every Hour",
-                    CTRANS_STATUS_CODE = "01",
-                    CTRANS_STATUS_NAME = "Closed"
-                },
-                new PMR00601DataDTO
-                {
-                    CCOMPANY_ID = "COMP01",
-                    CPROPERTY_ID = "PROP01",
-                    CBUILDING_ID = "BLD01",
-                    CBUILDING_NAME = "Building Jakarta",
-                    CDEPT_CODE = "DPT_A",
-                    CDEPT_NAME = "Department Jakarta Pusat",
-                    CTENANT_ID = "MU01",
-                    CTENANT_NAME = "Muhammad Andika Putra",
-                    CINVOICE_NO = "INV-REF-AGRE-1",
-                    CINVOICE_DATE = "1 Jan 2024",
-                    NINVOICE_AMOUNT = 100000000,
-                    NINVOICE_TAX_AMOUNT = 100000000,
-                    NINVOICE_TOTAL_AMOUNT = 200000000,
-                    CAGREEMENT_NO = "REF-AGRE-1",
-                    CAGREEMENT_DATE = "1 Jan 2024",
-                    CCHARGE_ID = "CHG01",
-                    CCHARGE_NAME = "Overtime Weekday(s)",
-                    CUNIT_ID = "Unit_A",
-                    CUNIT_NAME = "Unit Sederhana",
-                    COVERTIME_DATE = "1 Jan 2024",
-                    CTIME_IN = "18:00",
-                    CTIME_OUT = "21:00",
-                    CTENURE = "3H 0M",
-                    NUNIT_PRICE = 100000000,
-                    NUNIT_ACTUAL_AMOUNT = 100000000,
-                    CNOTE = "Every Hour",
-                    CTRANS_STATUS_CODE = "01",
-                    CTRANS_STATUS_NAME = "Closed"
-                },
-                new PMR00601DataDTO
-                {
-                    CCOMPANY_ID = "COMP01",
-                    CPROPERTY_ID = "PROP01",
-                    CBUILDING_ID = "BLD01",
-                    CBUILDING_NAME = "Building Jakarta",
-                    CDEPT_CODE = "DPT_A",
-                    CDEPT_NAME = "Department Jakarta Pusat",
-                    CTENANT_ID = "MU01",
-                    CTENANT_NAME = "Muhammad Andika Putra",
-                    CINVOICE_NO = "INV-REF-AGRE-1",
-                    CINVOICE_DATE = "1 Jan 2024",
-                    NINVOICE_AMOUNT = 100000000,
-                    NINVOICE_TAX_AMOUNT = 100000000,
-                    NINVOICE_TOTAL_AMOUNT = 100000000,
-                    CAGREEMENT_NO = "REF-AGRE-1",
-                    CAGREEMENT_DATE = "1 Jan 2024",
-                    CCHARGE_ID = "CHG01",
-                    CCHARGE_NAME = "Overtime Weekday(s)",
-                    CUNIT_ID = "Unit_A",
-                    CUNIT_NAME = "Unit Sederhana",
-                    COVERTIME_DATE = "1 Jan 2024",
-                    CTIME_IN = "18:00",
-                    CTIME_OUT = "21:00",
-                    CTENURE = "3H 0M",
-                    NUNIT_PRICE = 100000000,
-                    NUNIT_ACTUAL_AMOUNT = 100000000,
-                    CNOTE = "Every Hour",
-                    CTRANS_STATUS_CODE = "01",
-                    CTRANS_STATUS_NAME = "Closed"
-                },
-                // Data untuk tenant kedua
-                new PMR00601DataDTO
-                {
-                    CCOMPANY_ID = "COMP01",
-                    CPROPERTY_ID = "PROP01",
-                    CBUILDING_ID = "BLD01",
-                    CBUILDING_NAME = "Building Jakarta",
-                    CDEPT_CODE = "DPT_A",
-                    CDEPT_NAME = "Department Jakarta Pusat",
-                    CTENANT_ID = "AU01",
-                    CTENANT_NAME = "Aurelius Irvin",
-                    CINVOICE_NO = "INV-REF-AGRE-2",
-                    CINVOICE_DATE = "1 Jan 2024",
-                    NINVOICE_AMOUNT = 100000000,
-                    NINVOICE_TAX_AMOUNT = 100000000,
-                    NINVOICE_TOTAL_AMOUNT = 100000000,
-                    CAGREEMENT_NO = "REF-AGRE-2",
-                    CAGREEMENT_DATE = "1 Jan 2024",
-                    CCHARGE_ID = "CHG01",
-                    CCHARGE_NAME = "Overtime Weekday(s)",
-                    CUNIT_ID = "Unit_B",
-                    CUNIT_NAME = "Unit Besar",
-                    COVERTIME_DATE = "1 Jan 2024",
-                    CTIME_IN = "18:00",
-                    CTIME_OUT = "21:00",
-                    CTENURE = "3H 0M",
-                    NUNIT_PRICE = 100000000,
-                    NUNIT_ACTUAL_AMOUNT = 100000000,
-                    CNOTE = "Every Hour",
-                    CTRANS_STATUS_CODE = "01",
-                    CTRANS_STATUS_NAME = "Closed"
-                },
-                new PMR00601DataDTO
-                {
-                    CCOMPANY_ID = "COMP01",
-                    CPROPERTY_ID = "PROP01",
-                    CBUILDING_ID = "BLD01",
-                    CBUILDING_NAME = "Building Jakarta",
-                    CDEPT_CODE = "DPT_A",
-                    CDEPT_NAME = "Department Jakarta Pusat",
-                    CTENANT_ID = "AU01",
-                    CTENANT_NAME = "Aurelius Irvin",
-                    CINVOICE_NO = "INV-REF-AGRE-2",
-                    CINVOICE_DATE = "1 Jan 2024",
-                    NINVOICE_AMOUNT = 100000000,
-                    NINVOICE_TAX_AMOUNT = 100000000,
-                    NINVOICE_TOTAL_AMOUNT = 100000000,
-                    CAGREEMENT_NO = "REF-AGRE-2",
-                    CAGREEMENT_DATE = "1 Jan 2024",
-                    CCHARGE_ID = "CHG01",
-                    CCHARGE_NAME = "Overtime Weekday(s)",
-                    CUNIT_ID = "Unit_B",
-                    CUNIT_NAME = "Unit Besar",
-                    COVERTIME_DATE = "1 Jan 2024",
-                    CTIME_IN = "18:00",
-                    CTIME_OUT = "21:00",
-                    CTENURE = "3H 0M",
-                    NUNIT_PRICE = 100000000,
-                    NUNIT_ACTUAL_AMOUNT = 100000000,
-                    CNOTE = "Every Hour",
-                    CTRANS_STATUS_CODE = "01",
-                    CTRANS_STATUS_NAME = "Closed"
+                    for (int t = 0; t < tenants.Count; t++) // Tenants
+                    {
+                        for (int i = 0; i < 2; i++) // Invoices per Tenant
+                        {
+                            for (int a = 0; a < 2; a++) // Agreements per Invoice
+                            {
+                                for (int c = 0; c < charges.Count; c++) // Charges per Agreement
+                                {
+                                    loRtn.ReportDataDTO.Data.Add(new PMR00601DataDTO
+                                    {
+                                        CCOMPANY_ID = "COMP001",
+                                        CPROPERTY_ID = "PROP001",
+                                        CBUILDING_ID = $"BLD_{b}",
+                                        CBUILDING_NAME = $"Building {b}",
+                                        CDEPT_CODE = $"DPT_{d}",
+                                        CDEPT_NAME = $"Department {d}",
+                                        CTENANT_ID = $"TENANT{t + 1}",
+                                        CTENANT_NAME = tenants[t],
+                                        CINVOICE_NO = invoices[i + (t * 2)],
+                                        CINVOICE_DATE = "20240101",
+                                        NINVOICE_AMOUNT = amount,
+                                        NINVOICE_TAX_AMOUNT = amount,
+                                        NINVOICE_TOTAL_AMOUNT = 2 * amount,
+                                        CAGREEMENT_NO = agreements[a + (i * 2)],
+                                        CAGREEMENT_DATE = "20240101",
+                                        CCHARGE_ID = charges[c],
+                                        CCHARGE_NAME = charges[c] == "CHG01" ? "Overtime Weekday(s)" : "Overtime Weekend(s)",
+                                        CUNIT_ID = $"UNIT_{(c % 2 == 0 ? 'A' : 'B')}",
+                                        CUNIT_NAME = $"Unit {(c % 2 == 0 ? "Sederhana" : "Mewah")}",
+                                        COVERTIME_DATE = "20240101",
+                                        CTIME_IN = "18:00",
+                                        CTIME_OUT = "21:00",
+                                        CTENURE = "3H 0M",
+                                        NUNIT_PRICE = amount,
+                                        NUNIT_ACTUAL_AMOUNT = amount,
+                                        CNOTE = "Every Hour",
+                                        CTRANS_STATUS_CODE = "01",
+                                        CTRANS_STATUS_NAME = "Closed"
+                                    });
+
+                                    amount += 100000000; // Increase amount to ensure distinct values
+                                }
+                            }
+                        }
+                    }
                 }
-            };
+            }
             return loRtn;
         }
     }
